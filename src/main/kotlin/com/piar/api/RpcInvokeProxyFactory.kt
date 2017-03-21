33efbed044228af452fc3f2b9f8fb166d9ebd86e
邢轲 <xingke@meituan.com>
2017-03-21 11:04:20 +0800

    方法动态代理

---
+++ src/main/kotlin/com/piar/api/RpcInvokeProxyFactory.kt
@@ -0,0 +1,23 @@
+package com.piar.api
+
+import java.lang.reflect.Proxy
+
+/**
+ * Created by xingke on 2017/3/21.
+ */
+class RpcInvokeProxyFactory {
+
+    companion object {
+        @JvmStatic
+        fun <T> getProxy(clazz: Class<T>): T {
+            return clazz.cast(
+                    Proxy.newProxyInstance( ClassLoader.getSystemClassLoader(), arrayOf(clazz)) {
+                        proxy, method, args -> println("你正在被代理,这里将要被替换成调用远程的RPC方法 args: " + args?.joinToString(","))
+                        val ret = args[0] as Int + args[1] as Int
+                        println("result: $ret")
+                        ret
+                    })
+        }
+    }
+
+}
---
+++ src/test/java/com/piar/test/testLocalService.java
@@ -0,0 +1,10 @@
+package com.piar.test;
+
+/**
+ * Created by xingke on 2017/3/21.
+ */
+public interface testLocalService {
+
+    int add(int a, int b);
+
+}
